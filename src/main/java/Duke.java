a19ddd578408e1105565dd55093bb6551b970b18
claysmilesoil <e0323492.u.nus.edu>
2019-08-19 20:43:00 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,12 +7,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "_________________________\n";
+        String[] itemlist = new String[100];
+
         System.out.println("Hello from\n" + logo);
-        System.out.println(line + "What can I do for you?\nCurrently all I can do is echo.\n"+ line);
+        System.out.println(line + "Hello, I'm Duke!\nWhat can I do for you?\n" +
+                "Currently I can create a list for you.\n"+ line);
         Scanner inp = new Scanner(System.in);
         String input = inp.nextLine();
+        int listsize = 0;
         while (!input.equals("bye")) {
-            System.out.println(line + input + "\n" + line);
+            if (input.equals("list")) {
+                System.out.print(line);
+                if (listsize == 0) {
+                    System.out.println("There is no item in the list.");
+                } else {
+                    for (int i = 0; i < listsize; i++) {
+                        System.out.println(i + 1 + ". " + itemlist[i]);
+                    }
+                }
+                System.out.println(line);
+            } else {
+                itemlist[listsize++] = input;
+                System.out.println(line + "added: " + input + "\n" + line);
+            }
             input = inp.nextLine();
         }
         System.out.println(line + "Bye, see you soon!\n" + line);
