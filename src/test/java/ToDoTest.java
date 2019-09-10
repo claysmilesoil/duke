57e1775a5715d3531b0b650c249eec01acb55a27
claysmilesoil <e0323492.u.nus.edu>
2019-09-10 23:57:32 +0800

    Rename file

--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 /**
  * Test to check if the fields of a ToDo object in a list are instantiated correctly
  */
-public class addToDoTest {
+public class ToDoTest{
     @Test
     public void addToDoTest() {
         ToDo.addToDo("       Finish JUnit tests     "); // extra spaces to check trim() method
